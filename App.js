de51d29920deca378d0ee70d3cc1c19e287830be
Tanishq Singh Anand <60284327+TanishqSinghAnand@users.noreply.github.com>
2020-09-12 23:27:01 +0530

    Added the correct Version

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import HomeScreen from './screen/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 
 export default class App extends Component{
   render(){
